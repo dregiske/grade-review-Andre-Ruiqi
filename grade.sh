425e85f4cfbbcbc689fb93fbf83fadb83e508fdc
Andre <146780188+dregiske@users.noreply.github.com>
2024-05-28 16:02:05 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,8 +5,36 @@ rm -rf grading-area
 
 mkdir grading-area
 
+# step 1
 git clone $1 student-submission
 echo 'Finished cloning'
+
+#step 2
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "File was submitted"
+else
+    echo "File was not submitted"
+    exit
+fi
+
+# step 3
+# cp (files to copy) (directory to copy)
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area # -r recursively copies files from a directory into another given directory.
+echo "Files copied into grading-area"
+
+# step 4
+cd grading-area
+javac -cp $CPATH *.java
+if [[ $? -ne 0 ]]; then
+    echo "java files had issues compiling"
+else
+    echo "java files compiled"
+fi
+
+# step 5
+java -cp .:"$CPATH" org.junit.runner.JUnitCore TestListExamples
 
 
 # Draw a picture/take notes on the directory structure that's set up after
